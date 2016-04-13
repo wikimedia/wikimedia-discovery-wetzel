7399b6c746b1e938a209a6927f544c8d12df9529
Mikhail Popov <mikhail@mpopov.com>
2016-04-13 15:42:03 -0700

    Add spline smoothing option
    Change-Id: I8bcd0c22cf2c7e28da0e8de33494ae2f14d22fd3

--- ui.R
+++ ui.R
@@ -25,7 +25,7 @@ sidebar <- dashboardSidebar(
     menuItem(text = "Geographic breakdown", tabName = "geo_breakdown"),
     menuItem(text = "Global Settings",
              selectInput(inputId = "smoothing_global", label = "Smoothing", selectize = TRUE, selected = "day",
-                         choices = c("No Smoothing" = "day", "Weekly Median" = "week", "Monthly Median" = "month")),
+                         choices = c("No Smoothing" = "day", "Weekly Median" = "week", "Monthly Median" = "month", "Splines" = "gam")),
              selectInput(inputId = "timeframe_global", label = "Time Frame", selectize = TRUE, selected = "",
                          choices = c("All available data" = "all", "Last 7 days" = "week", "Last 30 days" = "month",
                                      "Last 90 days" = "quarter", "Custom" = "custom")),
