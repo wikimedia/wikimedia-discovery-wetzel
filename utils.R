aea0067900761e35584781401956cdfa2bf771d2
Mikhail Popov <mpopov@wikimedia.org>
2017-02-01 16:29:57 -0800

    Point to new datasets
    Bug: T150915
    Change-Id: Ia84140f46c9b83afa8909ff2d143a1d58de72f24

--- utils.R
+++ utils.R
@@ -1,41 +1,39 @@
 # Dependencies
-library(plyr)
 library(magrittr)
-library(dplyr)
-library(tidyr)
-library(reshape2)
-library(polloi)
-library(data.table)
 
 # Read in action data and write it into the global scope with sufficient formatting to be trivially
 # used in dygraphs.
 read_actions <- function() {
   # Read in data, split and rename it, and write it o
-  data <- polloi::read_dataset("maps/actions_per_tool.tsv") %>%
-    dplyr::rename(date = timestamp)
-  data <- split(data[, c(1, 3, 4)], data$feature)
-  usage_data <<- lapply(data, reshape2::dcast, formula = date ~ variable, fun.aggregate = sum)
+  usage_data <<- polloi::read_dataset("discovery/maps/actions_per_tool.tsv", col_types = "Dcci") %>%
+    dplyr::filter(!is.na(feature), !is.na(action), !is.na(events)) %>%
+    { split(.[, c(1, 3, 4)], .$feature) } %>%
+    lapply(tidyr::spread_, key = "action", value = "events", fill = 0)
   return(invisible())
 }
 
 # Read in user count
 read_users <- function() {
-  data <- polloi::read_dataset("maps/users_per_feature.tsv") %>%
-    dplyr::rename(date = timestamp)
-  interim <- reshape2::dcast(data, formula = date ~ variable, fun.aggregate = sum)
-  interim[is.na(interim)] <- 0
-  user_data <<- interim
+  user_data <<- polloi::read_dataset("discovery/maps/users_per_feature.tsv", col_types = "Dci") %>%
+    dplyr::filter(!is.na(feature), !is.na(users)) %>%
+    tidyr::spread(feature, users, fill = 0)
   return(invisible())
 }
 
 read_tiles <- function() {
-  new_tiles_automata <<- as.data.table(polloi::read_dataset("maps/tile_aggregates_with_automata.tsv", col_types = "Dcidcciidiii"))
-  new_tiles_no_automata <<- as.data.table(polloi::read_dataset("maps/tile_aggregates_no_automata.tsv", col_types = "Dcidcciidiii"))
+  new_tiles_automata <<- polloi::read_dataset("discovery/maps/tile_aggregates_with_automata.tsv", col_types = "Dcidcciidiii") %>%
+    dplyr::filter(!is.na(total), !is.na(users)) %>%
+    data.table::as.data.table()
+  new_tiles_no_automata <<- polloi::read_dataset("discovery/maps/tile_aggregates_no_automata.tsv", col_types = "Dcidcciidiii") %>%
+    dplyr::filter(!is.na(total), !is.na(users)) %>%
+    data.table::as.data.table()
   return(invisible())
 }
 
 read_countries <- function(){
-  interim <- polloi::read_dataset("maps/users_by_country.tsv")
-  interim$users <- interim$users*100
-  country_data <<- reshape2::dcast(interim, formula = date ~ country, fun.aggregate = sum)
+  country_data <<- polloi::read_dataset("discovery/maps/users_by_country.tsv", col_types = "Dcd") %>%
+    dplyr::filter(!is.na(country), !is.na(users)) %>%
+    dplyr::mutate(users = users * 100) %>%
+    tidyr::spread(country, users, fill = 0)
+  return(invisible())
 }
